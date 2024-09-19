c8ab741c68b11595b6bf5d3808981026ba9bc03a
Stas <stasfuryus@gmail.com>
2024-09-19 15:53:23 +0300

    add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель пользователя."""
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name="email", help_text="email address")
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
