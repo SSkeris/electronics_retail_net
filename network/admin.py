04a6c1fa05091e10a290a4c072d61030e91a297d
Stas <stasfuryus@gmail.com>
2024-09-20 18:10:01 +0300

    add NetworkNode admin

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from models import NetworkNode
 
-# Register your models here.
+
+class NetworkNodeAdmin(admin.ModelAdmin):
+    """Настройка админ панели модели 'NetworkNode'."""
+
+    list_display = ('title', 'contact', 'level', 'debt', 'created_at')
+    list_filter = ('contact__city',)
+    search_fields = ('title', 'contact__city')
+
+    def clear_debt(self, queryset):
+        """Убирает задолженность перед поставщиком."""
+
+        queryset.update(debt=0)
+
+    clear_debt.short_description = "Очистить задолженность перед поставщиком"
+
+    actions = [clear_debt]
+
+
+admin.site.register(NetworkNode, NetworkNodeAdmin)
